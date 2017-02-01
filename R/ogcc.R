6b1febde2bd3d722286c67d8da0981418d8dd138
aymanabuelela <ayman.elkhodiery@kaust.edu.sa>
2017-02-01 20:44:22 +0300

    updating the R/ogcc.R file and rebuilding the package

--- R/ogcc.R
+++ R/ogcc.R
@@ -3,9 +3,10 @@
 #' \code{ogcc} performs a group of cancer class predictions based on the expression of O-glycan-forming glycosyltransferase genes.
 #'
 #' @param x a numeric data frame or matrix of expression data where columns are the features and rows are cases/samples. The set of features must contain the minimum set of features for the specified model. See `Details`.
+#' @param d a string indicating the RNA-Seq measurement type; either 'RSEM' or 'RPKM'. 'RSEM' by default.
 #' @param model a character string indicating the model to perform the prediction task. It must be one of the following: "normal_tumor", "normal_tumor_reduced", "normal_tumor_types", "normal_tumor_types_reduced", "types" and "types_reduced".
 #' @param output a character value indicating the form of prediction output of the model. It must be either "raw" or "probs".
-#' @return a character in case of output = "raw" or a dataframe of class probalibilities in case of output = "probs"
+#' @return a character vector in case of output = "raw" or a dataframe of class probalibilities in case of output = "probs"
 #'
 #' @author Ayman Abuelela; ayman.elkhodiery@kaust.edu.sa
 #'
@@ -20,11 +21,7 @@
 #'
 #' \enumerate{
 #'       \item model \strong{\code{normal_tumor}}: predicts whether a sample is normal or cancer based on the expression profile of the OGFGs.
-#'       \item model \strong{\code{normal_tumor_reduced}}: the same as model \code{normal_tumor} but the features were reduced to the minimum set to bring 95\% accuracy.
-#'       \item model \strong{\code{normal_tumor_types}}: predicts whether the sample is normal or cancer in addition tissue type prediction.
-#'       \item model \strong{\code{normal_tumor_types_reduced}}: the same as model \code{normal_tumor_types} but the features were reduced to the minimum set to bring 95\% accuracy.
 #'       \item model \strong{\code{types}}: predicts the cancer type.
-#'       \item model \strong{\code{types_reduced}}: the same as model \code{types} but the features were reduced to the minimum set to bring 95\% accuracy.
 #' }
 #'
 #' @importFrom stats predict
@@ -32,7 +29,7 @@
 #' @seealso \code{\link{getMSF}} and \code{\link{getLabels}}
 #' @export
 
-ogcc <- function(x, model = "types", output = "raw") {
+ogcc <- function(x, model = "types", d = "RSEM", output = "raw") {
     ## check the class of x
     if (!is.matrix(x) & !is.data.frame(x)) {
         stop("ERROR: x must be a matrix or data frame")
@@ -42,25 +39,36 @@ ogcc <- function(x, model = "types", output = "raw") {
     apply(x, 2, function(x) if (is.numeric(x)) {} else {stop("ERROR: x must have numeric values")})
 
     ## check that the model name is correct
-    model_names <- names(prediction_models)
+    model_names <- c('types', 'normal_tumor')
     if (!(model %in% model_names)) {
-        stop('ERROR: model name is not found. It must be one of the following: "normal_tumor", "normal_tumor_reduced", "normal_tumor_types", "normal_tumor_types_reduced", "types" and "types_reduced".')
+        stop("ERROR: model name is not found. It must be one of the following: 'normal_tumor' and 'types'.")
     }
 
     ## check that the data set has the minimum set of features
-    msf <- getMSF(model = model)
+    msf <- getMSF(model = model, d = d)
     if (length(intersect(msf, colnames(x))) != length(msf)) {
         stop("ERROR: x does NOT have the minimum set of required features")
     }
 
+    ## check d
+    d_ <- c('RSEM', 'RPKM')
+    if (!(d %in% d_)) {
+        stop("ERROR: d must be either 'RPKM' or 'RSEM'.")
+    }
+
     ## get model object
-    model_obj <- prediction_models[[model]]
+    if (d == 'RSEM') {
+        model_obj <- rsem_pred[[model]]
+    } else {
+        model_obj <- rpkm_pred[[model]]
+    }
+
 
     ## determine the preprocessing object
-    if (grepl("normal", model)) {
-        pp_obj <- preprocessing_models$pp_NT
+    if (d == 'RSEM') {
+        pp_obj <- rsem_pp[[model]]
     } else {
-        pp_obj <- preprocessing_models$pp_types
+        pp_obj <- rpkm_pp[[model]]
     }
 
     ## preprocessing
@@ -79,6 +87,7 @@ ogcc <- function(x, model = "types", output = "raw") {
 #' \code{getMSF} prints the minimum set of features required by a model from \code{ogcc}.
 #'
 #' @param model is a prediction model name from \code{\link{ogcc}}. See \code{model} argument.
+#' @param d a string indicating the RNA-Seq measurement type; either 'RSEM' or 'RPKM'. 'RSEM' by default.
 #'
 #' @return a character vector of the required features for a working model.
 #'
@@ -91,12 +100,17 @@ ogcc <- function(x, model = "types", output = "raw") {
 #' @seealso \code{\link{ogcc}} and \code{\link{getLabels}}
 #' @export
 
-getMSF <- function(model = "types") {
-    model_names <- names(prediction_models)
+getMSF <- function(model = "types", d = 'RSEM') {
+    model_names <- c('types', 'normal_tumor')
     if (model %in% model_names) {
-        return(prediction_models[[model]]$coefnames)
+        if (d == 'RSEM') {
+            model_obj <- rsem_pred[[model]]
+        } else {
+            model_obj <- rpkm_pred[[model]]
+        }
+        return(model_obj$coefnames)
     } else {
-        stop('ERROR: model name is not found. It must be one of the following: "normal_tumor", "normal_tumor_reduced", "normal_tumor_types", "normal_tumor_types_reduced", "types" and "types_reduced".')
+        stop("ERROR: model name is not found. It must be one of the following: 'normal_tumor' and 'types'.")
     }
 }
 
@@ -116,18 +130,22 @@ getMSF <- function(model = "types") {
 #' @seealso \code{\link{ogcc}} and \code{\link{getMSF}}
 #' @export
 
-getLabels <- function(model = "types") {
-    model_names <- names(prediction_models)
+getLabels <- function(model = "types", d = 'RSEM') {
+    model_names <- c('types', 'normal_tumor')
     if (model %in% model_names) {
-        return(prediction_models[[model]]$finalModel$classes)
+        if (d == 'RSEM') {
+            model_obj <- rsem_pred[[model]]
+        } else {
+            model_obj <- rpkm_pred[[model]]
+        }
+        return(model_obj$finalModel$classes)
     } else {
-        stop('ERROR: model name is not found. It must be one of the following: "normal_tumor", "normal_tumor_reduced", "normal_tumor_types", "normal_tumor_types_reduced", "types" and "types_reduced".')
+        stop("ERROR: model name is not found. It must be one of the following: 'normal_tumor' and 'types'.")
     }
 }
 
 #' @importFrom utils data
 .onLoad <- function(libname, pkgname) {
     ## load modelsh
-    #path <- file.path(find.package(pkgname), "model.RData")
     data(model, package = pkgname, envir = parent.env(environment()))
 }
